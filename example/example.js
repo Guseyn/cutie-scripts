7910b938ba4f8cfe41999d38022113223b2c40e0
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-19 22:10:42 +0400

    fix example

--- example/example.js
+++
@@ -1,9 +0,0 @@
-const { ExecutedScripts } = require('./../index')
-
-new ExecutedScripts(
-	'node', 'js', './example/test/test.js', './example/test/dir1'
-).after(
-  new ExecutedScripts(
-    'node', 'js', './example/test/dir2'
-  )
-).call() // or just new ExecutedTests('./example/test').call()
